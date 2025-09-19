ee2bff289cb73a344a5c1ae032670621bd4ca8b2
cmcevoy0405 <callum.mcevoy01@gmail.com>
2025-09-19 17:00:20 +0100

    Added percentage

---
+++ pga_dbt/models/int_pga__stats.sql
@@ -0,0 +1,26 @@
+with staging_stats as (
+    select * 
+    from {{ ref('stg_pga__stats')}}
+)
+select 
+    player_id,
+    year,
+    MAX(CASE WHEN stat_name = 'avg_score' then stat_value end) as avg_score,
+    MAX(CASE WHEN stat_name = 'top_10' then stat_value end) as top_10,
+    MAX(CASE WHEN stat_name = 'drive_dist' then stat_value end) as drive_dist,
+    MAX(CASE WHEN stat_name = 'drive_acc' then stat_value end) as drive_acc,
+    MAX(CASE WHEN stat_name = 'putts_per_round' then stat_value end) as putts_per_round,
+    MAX(CASE WHEN stat_name = 'scramble' then stat_value end) as scramble,
+    MAX(CASE WHEN stat_name = 'gir' then stat_value end) as gir,
+    MAX(CASE WHEN stat_name = 'bounce_back' then stat_value end) as bounce_back,
+    MAX(CASE WHEN stat_name = 'strokes_gained' then stat_value end) as strokes_gained,
+    MAX(CASE WHEN stat_name = 'strokes_gained_ot' then stat_value end) as strokes_gained_ot,
+    MAX(CASE WHEN stat_name = 'strokes_gained_arg' then stat_value end) as strokes_gained_arg,
+    MAX(CASE WHEN stat_name = 'strokes_gained_ttg' then stat_value end) as strokes_gained_ttg,
+    MAX(CASE WHEN stat_name = 'strokes_gained_atg' then stat_value end) as strokes_gained_atg,
+    MAX(CASE WHEN stat_name = 'strokes_gained_putt' then stat_value end) as strokes_gained_putt,
+    MAX(CASE WHEN stat_name = 'par3_score' then stat_value end) as par3_score,
+    MAX(CASE WHEN stat_name = 'par4_score' then stat_value end) as par4_score,
+    MAX(CASE WHEN stat_name = 'par5_score' then stat_value end) as par5_score
+from staging_stats
+group by player_id, year
--- pga_dbt/models/staging/stg_pga__stats.sql
+++ pga_dbt/models/staging/stg_pga__stats.sql
@@ -8,6 +8,9 @@ select
     cast((rank) as int) as rank,
     cast(stat_id as int) as stat_id,
     trim(cast(stat_name as string)) as stat_name, 
-    cast(stat_value as float64) as stat_value,
+    case
+        when right(stat_value, 1) = '%' then cast(replace(stat_value, '%', '') as float64) / 100
+        else cast(stat_value as float64)
+    end as stat_value,
     cast(scraped_at as datetime) as scraped_at
 from source
